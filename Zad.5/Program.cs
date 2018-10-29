6b3d0e6fa6b438f7505b4eb8c5acf33a32071adc
Beihan <beihan99@mail.bg>
2018-10-29 23:07:05 +0200

    Udpate tasks

--- Zad.5/Program.cs
+++ Zad.5/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Zad._5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine(" Добър ден !");
         }
     }
 }
